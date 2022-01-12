f2e00286c68eeb735fd4a179c855cc89754956f8
Gabriel Nwokolo <92697325+LordTsagE@users.noreply.github.com>
2022-01-12 12:49:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a = (two_digit_number[0])
+c = int(a)
+b = (two_digit_number[1])
+d = int(b)
+print(c+d)
+
+
+
+
+
+
+
